2a2e4f414f8fa980d3a2cac6cd496d9ec6466a67
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-25 13:25:36 +0500

    add product correct and compelete code added

--- addProduct.js
+++ addProduct.js
@@ -8,31 +8,31 @@ const storageRef = storage.ref();
 // initialize  database
 const database = firebase.database();
 const addProduct = () => {
-    let fileInput = document.getElementById('file').value;
+    let fileInput = document.getElementById('file');
     let file = fileInput.files[0];
-    uploadFile(file);
-
-}
-const uploadFile = (file) => {
-    let product = {
-        productName: document.getElementById('itemName').value,
-        productDscrptn: document.getElementById('itemDcp').value,
-        productUnit: document.getElementById('itemQuantity').value,
-        productPrice: document.getElementById('itemPrice').value,
-        productImage:''
+                uploadFile(file);
     }
-    if (file) {
-        const uploadFolder = storageRef.child('uploaded_Images');
-        const imageRef = uploadFolder.child(file.name);
-        imageRef.put(file).then((snapshot) => {
-            console.log("file uploaded succesfully")
-
-        })
-        imageRef.getDownloadURL().then((url)=>{
-            console.log("download URL of Product :",url);
-            product.productImage=url;
-            console.log(product)
-        } )
+    const uploadFile = (file) => {
+        let product = {
+            productName: document.getElementById('itemName').value,
+            productDscrptn: document.getElementById('itemDcp').value,
+            productUnit: document.getElementById('itemQuantity').value,
+            productPrice: document.getElementById('itemPrice').value,
+            productImage: ''
+        }
+        if (file) {
+            const uploadFolder = storageRef.child('uploaded_Images');
+            const imageRef = uploadFolder.child(file.name);
+            imageRef.put(file).then((snapshot) => {
+                console.log("file uploaded succesfully")
+    
+            })
+            imageRef.getDownloadURL().then((url) => {
+                console.log("download URL of Product :", url);
+                product.productImage = url;
+                console.log(product)
+            })
+        }
+    
     }
-
-}
+    
